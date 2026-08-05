840988fabc9e96de25c03ccba451e0ba0c8125a3
Claude Durocher <claude.durocher@gmail.com>
2026-08-04 22:50:46 -0400

    Add php-cs-fixer config for the already-vendored tool

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,47 @@
+<?php
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        'array_syntax' => [
+            'syntax' => 'short'
+        ],
+        'combine_consecutive_unsets' => true,
+        'heredoc_to_nowdoc' => true,
+        'no_extra_blank_lines' => [
+            'tokens' => [
+                'break',
+                'continue',
+                'extra',
+                'return',
+                'throw',
+                'use',
+                'parenthesis_brace_block',
+                'square_brace_block',
+                'curly_brace_block'
+            ]
+        ],
+        'no_unreachable_default_argument_value' => true,
+        'no_useless_else' => true,
+        'no_useless_return' => true,
+        'ordered_class_elements' => true,
+        'ordered_imports' => true,
+        'php_unit_strict' => true,
+        'phpdoc_order' => true,
+        // 'psr4' => true,
+        'strict_comparison' => true,
+        'strict_param' => true,
+        'concat_space' => [
+            'spacing' => 'one'
+        ],
+    ])
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+            ->exclude([
+                'vendor',
+                'var',
+                'web',
+                'tpl_c'
+            ])
+            ->in(__DIR__)
+    )
+;
